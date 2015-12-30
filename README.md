2e5f72cfcdf825c74bf041a5d96f13adf4dfabf3
Yousuf Hussain <yousuf.1991@hotmail.com>
2015-12-29 19:00:38 -0500

    -Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # seam-carving-meteor
-A seam carving implementation on Meteor for anyone who wants to edit a photo quick
+A seam carving implementation on Meteor for anyone who wants to edit a photo quick.
+Actual site -
+It is an implementation of http://www.cs.princeton.edu/courses/archive/spr13/cos226/assignments/seamCarving.html
+just done on Javascript with Meteor.
+
--- hello.js
+++ hello.js
@@ -1,44 +1,68 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  //Session.setDefault('counter', 0);
+    // counter starts at 0
+    //Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    countAmount: function () {
+    Template.hello.helpers({
+        countAmount: function () {
 
-    }
-  });
+        }
+    });
 
-  Template.hello.events({
-    'change #file': function (event, template) {
-      var canvas1 = document.getElementById("myCanvas");
-      var canvasSobel = document.getElementById('yourCanvas');
-      var contextSobel = canvasSobel.getContext("2d");
-      var url = window.URL.createObjectURL(event.target.files[0]);
-      var image = new Image();
-      image.src = url;
-      image.onload = function () {
-        var w = image.width;
-        var h = image.height;
-        canvas1.width = w;
-        canvas1.height = h;
-        var horw = $('input[name="dimension"]:checked').val();
-        //var heightOrWidth = template.find("dimension").value;
-        console.log(horw);
-        var amountElement = template.find("#amount");
-        canvasSobel.width = w;
-        canvasSobel.height = h;
-        var ctx = canvas1.getContext("2d");
-        ctx.drawImage(image, 0, 0);
-        var imageData = ctx.getImageData(0, 0, w, h);
-        var sobelImageData = Sobel(imageData);
-        contextSobel.putImageData(sobelImageData, 0, 0);
-      }
-    }
-  });
+    Template.hello.events({
+        'change #file': function (event, template) {
+            var canvas1 = document.getElementById("myCanvas");
+            var canvasSobel = document.getElementById('yourCanvas');
+            var contextSobel = canvasSobel.getContext("2d");
+            var url = window.URL.createObjectURL(event.target.files[0]);
+            var image = new Image();
+            image.src = url;
+            image.onload = function () {
+                var w = image.width;
+                var h = image.height;
+                canvas1.width = w;
+                canvas1.height = h;
+                var horw = $('input[name="dimension"]:checked').val();
+                //var heightOrWidth = template.find("dimension").value;
+                console.log(horw);
+                var amountElement = template.find("#amount");
+                canvasSobel.width = w;
+                canvasSobel.height = h;
+                var ctx = canvas1.getContext("2d");
+                ctx.drawImage(image, 0, 0);
+                var imageData = ctx.getImageData(0, 0, w, h);
+                var sobelImageData = Sobel(imageData);
+                contextSobel.putImageData(sobelImageData, 0, 0);
+            }
+        },
+        'click #carve-button': function (event, template) {
+            var amount = $("#amount").val();
+            var dim = $('input[name="dimension"]:checked').val();
+            var canvas1 = document.getElementById("myCanvas");
+            var ctx = canvas1.getContext("2d");
+            console.log(ctx);
+            if(ctx==undefined){
+                $("#errorMessageImage").show();
+                return false;
+            }
+            var imageData = ctx.getImageData(0, 0, ctx.width, ctx.height);
+            $("#errorMessageImage").hide();
+            if (dim == undefined) {
+                $("#errorMessageDim").show();
+                return false;
+            }
+            $("#errorMessageDim").hide();
+            if (amount == "") {
+                $("#errorMessageAmount").show();
+                return false;
+            }
+            $("#errorMessageAmount").hide();
+
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
